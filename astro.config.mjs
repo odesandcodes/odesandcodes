49a894a307f10e845c960c25c4e34ae34e03100a
neps-influx <nathaniel.epstein@influxmarketing.com>
2026-01-30 20:27:26 -0500

    tidy up page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // This is the "Switch" that prevents 404s on your AWS IP
-  base: process.env.DEPLOY_TARGET === 'caddy' ? '/odes' : '/',
+  // Hardcode this to root for now to eliminate the variable as a point of failure
+  base: '/', 
   outDir: './dist',
+  // Cloudflare doesn't need a site URL to render CSS, but it helps for absolute links
+  site: 'https://odesandcodes.pages.dev', 
 });
