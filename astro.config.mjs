bad84394163e0d8ba9a971decea839586c89fe33
nepsidehustle <nepsidehustle@gmail.com>
2026-02-06 12:23:42 -0500

    admin dash12

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  // We removed the cloudflare import/adapter to stop the build error
   site: 'https://odesandcodes.com',
   base: '/',
+  // Force Astro to look at the 'public' folder in your root
+  publicDir: 'public', 
 });
